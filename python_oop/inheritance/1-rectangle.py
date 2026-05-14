8dd3d3191b5c460f362f86f822153ee9d4486084
Sébastien Lamblin <seb.lamblin59@gmail.com>
2026-05-14 09:44:29 +0200

    1-rectangle.py

---
+++ python_oop/inheritance/1-rectangle.py
@@ -0,0 +1,28 @@
+#!/usr/bin/env python3
+"""
+Module rectangle
+
+Defines the Rectangle class.
+"""
+
+BaseGeometry = __import__('base_geometry').BaseGeometry
+
+
+class Rectangle(BaseGeometry):
+    """
+    Rectangle class that inherits from BaseGeometry.
+    """
+
+    def __init__(self, width, height):
+        """
+        Initializes a rectangle.
+
+        Args:
+            width (int): width of the rectangle
+            height (int): height of the rectangle
+        """
+        self.integer_validator("width", width)
+        self.integer_validator("height", height)
+
+        self.__width = width
+        self.__height = height
