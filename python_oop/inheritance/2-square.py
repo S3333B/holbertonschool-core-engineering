783cf935c9be5d742e45315d241698201ae1ae78
Sébastien Lamblin <seb.lamblin59@gmail.com>
2026-05-14 09:46:27 +0200

    2-square.py

---
+++ python_oop/inheritance/2-square.py
@@ -0,0 +1,27 @@
+#!/usr/bin/env python3
+"""
+Module square
+
+Defines the Square class.
+"""
+
+Rectangle = __import__('2-rectangle').Rectangle
+
+
+class Square(Rectangle):
+    """
+    Square class that inherits from Rectangle.
+    """
+
+    def __init__(self, size):
+        """
+        Initializes a square.
+
+        Args:
+            size (int): size of the square
+        """
+        self.integer_validator("size", size)
+
+        self.__size = size
+
+        super().__init__(size, size)
