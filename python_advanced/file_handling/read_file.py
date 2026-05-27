89ce00e0d92eaf576c5aa9771ba5b7f1b5bb6833
Sébastien Lamblin <seb.lamblin59@gmail.com>
2026-05-27 09:13:44 +0200

    read_file.py

--- python_advanced/file_handling/read_file.py
+++ python_advanced/file_handling/read_file.py
@@ -1,5 +1,8 @@
 #!/usr/bin/env python3
+
 def read_file(filename=""):
-    """Reads a text file (UTF8) and prints it to stdout"""
-    with open(filename, encoding="utf-8") as f:
-        print(f.read(), end="")
+    with open(filename, encoding="utf-8") as file:
+        print(file.read())
+
+if __name__ == "__main__":
+    read_file("my_file_0.txt")
