43837e12e7967b980b11ab83946fd932ed927fa7
Sébastien Lamblin <seb.lamblin59@gmail.com>
2026-05-27 09:47:22 +0200

    write_file.py

--- python_advanced/file_handling/write_file.py
+++ python_advanced/file_handling/write_file.py
@@ -1,6 +1,19 @@
 #!/usr/bin/env python3
+"""A function that writes a string to a text file (UTF8)
+and returns the number of characters written.
+"""
+
+
 def write_file(filename="", text=""):
-    """Write a string to a UTF-8 text file and return
-    the number of characters written."""
-    with open(filename, "w", encoding="utf-8") as file:
-        return file.write(text)
+    """Write a string to a UTF8 text file.
+
+    Args:
+        filename (str): Name of the file.
+        text (str): Text to write in the file.
+
+    Returns:
+        int: Number of characters written.
+    """
+    with open(filename, "w", encoding="utf-8") as my_file:
+        return my_file.write(text)
+    
