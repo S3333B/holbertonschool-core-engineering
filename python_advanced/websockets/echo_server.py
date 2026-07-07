d8f8042d5debdefbe4c0637ddc4fa746a994e1d7
Sébastien Lamblin <seb.lamblin59@gmail.com>
2026-07-07 10:09:07 +0200

    echo_server.py

--- python_advanced/websockets/echo_server.py
+++ python_advanced/websockets/echo_server.py
@@ -1,18 +1,27 @@
 #!/usr/bin/env python3
+"""
+Simple WebSocket echo server.
+"""
+
 import asyncio
 import websockets
 
-async def echo(websocket):
-	
-  async for message in websocket:
+
+async def connection_handler(websocket):
+    """
+    Receive messages from a client and send back the exact same message.
+    """
+    async for message in websocket:
         await websocket.send(message)
+
 
 async def main():
     """
-    Start the WebSocket server.
+    Start the WebSocket echo server.
     """
-    async with websockets.serve(echo, "localhost", 8765):
+    async with websockets.serve(connection_handler, "localhost", 8765):
         await asyncio.Future()
-        
+
+
 if __name__ == "__main__":
     asyncio.run(main())
